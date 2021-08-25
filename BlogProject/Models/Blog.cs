bb0657bc4b6c9323d00ac13935af21695c66c32d
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-08-25 14:24:45 -0500

    Create blog model

---
+++ BlogProject/Models/Blog.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters.", MinimumLength = 4)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at most {1} characters.", MinimumLength = 4)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
+        public DateTime Created { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
+        public DateTime? Updateded { get; set; }
+
+        [Display(Name="Blog Image")]
+        public byte[] ImageData { get; set; }
+        public string ImageType { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Image")]
+        [DataType(DataType.Upload)]
+        public IFormFile ImageFile { get; set; }
+
+    }
+}
